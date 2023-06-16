72a82873381d816b2c9ba77bea80787895fd5e30
NatasaIc <iconomou.gmail.com>
2023-06-16 09:51:45 +0200

    edit form deleted

--- partials/edit-garment-form.php
+++
@@ -1,7 +0,0 @@
-<!-- <form class="edit-form" action="form-handlers/update-sold-form.php" method="post">
-
-<label for="id" class="edit-heading">Plagg ID: </label>
-<label for="sold_status">Markera som såld:</label>
-<input type='hidden' name='garment_id' value="">
-<input type="submit" value="Köp">
-</form> -->
